7ffb576e57205900e9a808ed39f5072eb8c4bf25
Mykola Tunyk <mtunyk@gmail.com>
2020-10-14 00:36:08 +0300

    Use local data

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # CO2 Birthdate Website 
 
+## Prerequisites
+
+- [Node.js v14+](https://nodejs.org/en/download/current/)
+
+## About this project
+
 This is the respository where CO2Birthdate's website lives.  
-HTML, CSS, and JS powered documents, generated using Python.  
-Once you've forked/clowned/downloaded/etc the repo, run Python's `http.server`, which will get a localhost up and running for development.  
-Navigate to localhost in your browser of choice to see it in action/develop in the browser.
+HTML, CSS, and JS powered documents, generated using Python.
+
+Once you've forked/cloned/downloaded/etc the repository, install project dependencies with
+```
+npm i
+```
+then run
+```
+npm run dev
+```
+which will get a localhost up and running for development.  
+Navigate to localhost:3000 in your browser of choice to see it in action/develop in the browser.
--- pages/co2/[date].js
+++ pages/co2/[date].js
@@ -3,7 +3,7 @@ import { parse, format } from 'date-fns'
 
 import Layout from '../../layouts/layout'
 import { getCo2ByDate, getCo2Dates } from '../../lib/api'
-import { DATE_FORMAT } from '../../lib/constants'
+import { DATE_FORMAT, DEFAULT_DATE } from '../../lib/constants'
 
 const CO2Page = ({ date, ppm, difference, latest }) => (
   <>
@@ -15,7 +15,7 @@ const CO2Page = ({ date, ppm, difference, latest }) => (
       <p>Date: <time dateTime={date}>{date}</time></p>
       <p>On this date: <b>{ppm}</b> ppm CO<sub>2</sub></p>
       <p>Most recent measurement: <b>{latest.ppm}</b> ppm CO<sub>2</sub> on <time dateTime={latest.date}>{latest.date}</time></p>
-      <p>That's a change of <b>{difference}</b>%</p>
+      <p>That's a change of <b>{(difference > 0) && '+'}{difference}</b>%</p>
     </Layout>
   </>
 )
@@ -24,16 +24,15 @@ export const getStaticPaths = async () => {
   const dates = await getCo2Dates()
 
   return {
-    paths: dates?.map((date) => ({ params: { date } })),
+    paths: dates?.map(({ date }) => ({ params: { date } })),
     fallback: false,
   }
 }
 
 export const getStaticProps = async ({ params }) => {
-  const date = parse(params.date, DATE_FORMAT, new Date())
+  const date = parse(params.date, DATE_FORMAT, new Date(DEFAULT_DATE))
   const { co2, latest } = await getCo2ByDate(date)
-  const ppm = co2.ppm?.toFixed(1)
-  latest.ppm.toFixed(1)
+  const ppm = co2.ppm
   const difference = ( 100 * (latest.ppm - ppm) / ppm).toFixed(1)
 
   return {
